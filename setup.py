26ace814f3c2612e45c89947705124e347399066
ABHIJEETMISHRA15 <abhijeet010207@gmail.com>
2024-10-24 01:57:36 +0530

    folderCreated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Sarathi mediBot',
+    version= '0.0.0',
+    author= 'Abhijeet Mishra',
+    author_email= 'abhijeet010207@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
